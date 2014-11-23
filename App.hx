5f7e6e8825ab5e866f13ce4366cfc8c7fe2be484
tondy <dev@tondy.com>
2014-11-23 11:03:13 +0100

    test

---
+++ App.hx
@@ -0,0 +1,19 @@
+package;
+
+/**
+ * hxshell App
+ **/
+class App{
+
+	public function new()
+	{
+ 		
+	}// new()
+
+	public static function main() 
+	{
+ 		var n = new App();
+	}// main()
+
+}// App
+
